5b90e1fd0262136bf5e52e5fd3853b3b0c633f39
ShawnPad <59770535+ShawnPad@users.noreply.github.com>
2022-04-25 20:15:50 -0400

    add more tests

--- frontend/src/components/PlayerAppearance/AppearanceModal.test.tsx
+++ frontend/src/components/PlayerAppearance/AppearanceModal.test.tsx
@@ -1,15 +1,22 @@
-import {ChakraProvider, useDisclosure} from '@chakra-ui/react';
 import React, {useState} from "react";
 import {fireEvent, render} from "@testing-library/react";
+import {ChakraProvider, useDisclosure} from '@chakra-ui/react';
 import AppearanceModal from "./AppearanceModal";
 import {PlayerAppearance} from "../../classes/Player/Player";
 
 describe('AppearanceModal', () => {
+  const setState = jest.fn()
+
+  jest.mock('react', () => ({
+    ...jest.requireActual('react'),
+    useState: jest.fn(),
+  }))
+
   const Wrapper = () => {
     const {
       onClose: onCustomizeClose
     } = useDisclosure();
-    const [selectedAppearance, setSelectedAppearance] = useState<PlayerAppearance>({
+    const [selectedAppearance] = useState<PlayerAppearance>({
       hair: 0,
       pants: 0,
       shirt: 0,
@@ -19,7 +26,7 @@ describe('AppearanceModal', () => {
       <ChakraProvider>
         <React.StrictMode>
           <AppearanceModal appearance={selectedAppearance} isOpen
-                           onAppearanceUpdated={setSelectedAppearance} onClose={onCustomizeClose}/>
+                           onAppearanceUpdated={setState} onClose={onCustomizeClose}/>
         </React.StrictMode>
       </ChakraProvider>
     );
@@ -37,6 +44,24 @@ describe('AppearanceModal', () => {
 
     fireEvent.click(getByLabelText("hair-Short Black"));
     expect(getByLabelText("hair-Short Black")).toBeChecked()
+  });
+
+  it('click option and save saves', async () => {
+    const {getByText, getByLabelText} = render(<Wrapper/>)
+    fireEvent.click(getByLabelText("hair-Long Blonde"));
+    expect(getByLabelText("hair-Long Blonde")).toBeChecked()
+
+    fireEvent.click(getByText("Save"));
+    expect(setState).toHaveBeenCalledWith({"hair": 1, "pants": 0, "shirt": 0, "skin": 0});
+  });
+
+  it('click option and cancel does not save', async () => {
+    const {getByText, getByLabelText} = render(<Wrapper/>)
+    fireEvent.click(getByLabelText("hair-Long Blonde"));
+    expect(getByLabelText("hair-Long Blonde")).toBeChecked()
+
+    fireEvent.click(getByText("Cancel"));
+    expect(setState).toHaveBeenCalledTimes(0);
   });
 
 });
